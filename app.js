d58357b8802e701772ca04a7d527a75ecf63a1e5
labellatrix <sjshmerlis@gmail.com>
2016-12-26 19:11:50 -0500

    build your quiz app

---
+++ app.js
@@ -0,0 +1,211 @@
+// State object
+var state = {
+  questions: [
+    {
+      text: "How many teams are there in the NBA?",
+      choices: ["30", "25", "32", "28"],
+      correctChoiceIndex: 0
+    },
+    {
+      text: "Which team did Michael Jordan play for?",
+      choices: ["Detriot Pistons", "Chicago Bulls", "Boston Celtics", "Los Angeles Lakers"],
+      correctChoiceIndex: 1
+    },
+    {
+      text: "Who is the all-time NBA leading scorer?",
+      choices: ["Michael Jordan", "Kobe Bryant", "Kareem Abdul-Jabbar", "Karl Malone"],
+      correctChoiceIndex: 2,
+    },
+    {
+      text: "What is the minimum number of players a NBA roster must have?",
+      choices: ["10", "11", "9", "13"],
+      correctChoiceIndex: 3,
+    },
+    {
+      text: "Who is the all-time NBA leader in assists?",
+      choices: ["John Stockton", "Jason Kidd", "Steve Nash", "Jerry West"],
+      correctChoiceIndex: 0,
+    }
+  ],
+  praises : [
+    "Correct! Good job!",
+    "You're a real NBA Einstein!",
+    "Well done! You must dream basketball in your sleep!",
+    "Right answer! You deserve the NBA MVP!"
+  ],
+
+  admonishments: [
+    "Better luck next time.",
+    "Do you even like basketball?",
+    "How could you not know that? That was an easy one!"
+  ],
+  score: 0,
+  currentQuestionIndex: 0,
+  route: 'start',
+  lastAnswerCorrect: false,
+  feedbackRandom: 0
+};
+
+// State modification functions
+function setRoute(state, route) {
+  state.route = route;
+};
+
+function resetGame(state) {
+  state.score = 0;
+  state.currentQuestionIndex = 0;
+  setRoute(state, 'start');
+};
+
+function answerQuestion(state, answer) {
+  var currentQuestion = state.questions[state.currentQuestionIndex];
+  state.lastAnswerCorrect = currentQuestion.correctChoiceIndex === answer;
+  if (state.lastAnswerCorrect) {
+    state.score++;
+  }
+  selectFeedback(state);
+  setRoute(state, 'answer-feedback');
+};
+
+function selectFeedback(state) {
+  state.feedbackRandom = Math.random();
+};
+
+function advance(state) {
+  state.currentQuestionIndex++;
+  if (state.currentQuestionIndex === state.questions.length) {
+    setRoute(state, 'final-feedback');
+  }
+  else {
+    setRoute(state, 'question');
+  }
+};
+
+// Render functions
+function renderApp(state, elements) {
+  // default to hiding all routes, then show the current route
+  Object.keys(elements).forEach(function(route) {
+    elements[route].hide();
+  });
+  elements[state.route].show();
+
+  if (state.route === 'start') {
+      renderStartPage(state, elements[state.route]);
+  }
+  else if (state.route === 'question') {
+      renderQuestionPage(state, elements[state.route]);
+  }
+  else if (state.route === 'answer-feedback') {
+    renderAnswerFeedbackPage(state, elements[state.route]);
+  }
+  else if (state.route === 'final-feedback') {
+    renderFinalFeedbackPage(state, elements[state.route]);
+  }
+};
+
+// at the moment, `renderStartPage` doesn't do anything, because
+// the start page is preloaded in our HTML, but we've included
+// the function and used above in our routing system so that this
+// application view is accounted for in our system
+function renderStartPage(state, element) {
+};
+
+function renderQuestionPage(state, element) {
+  renderQuestionCount(state, element.find('.question-count'));
+  renderQuestionText(state, element.find('.question-text'));
+  renderChoices(state, element.find('.choices'));
+};
+
+function renderAnswerFeedbackPage(state, element) {
+  renderAnswerFeedbackHeader(state, element.find(".feedback-header"));
+  renderAnswerFeedbackText(state, element.find(".feedback-text"));
+  renderNextButtonText(state, element.find(".see-next"));
+};
+
+function renderFinalFeedbackPage(state, element) {
+  renderFinalFeedbackText(state, element.find('.results-text'));
+};
+
+function renderQuestionCount(state, element) {
+  var text = (state.currentQuestionIndex + 1) + "/" + state.questions.length;
+  element.text(text);
+};
+
+function renderQuestionText(state, element) {
+  var currentQuestion = state.questions[state.currentQuestionIndex];
+  element.text(currentQuestion.text);
+};
+
+function renderChoices(state, element) {
+  var currentQuestion = state.questions[state.currentQuestionIndex];
+  var choices = currentQuestion.choices.map(function(choice, index) {
+    return (
+      '<li>' +
+        '<input type="radio" name="user-answer" value="' + index + '" required>' +
+        '<label>' + choice + '</label>' +
+      '</li>'
+    );
+  });
+  element.html(choices);
+};
+
+function renderAnswerFeedbackHeader(state, element) {
+  var html = state.lastAnswerCorrect ?
+      "<h6 class='user-was-correct'>correct</h6>" :
+      "<h1 class='user-was-incorrect'>Wrooonnnngggg!</>";
+
+  element.html(html);
+};
+
+function renderAnswerFeedbackText(state, element) {
+  var choices = state.lastAnswerCorrect ? state.praises : state.admonishments;
+  var text = choices[Math.floor(state.feedbackRandom * choices.length)];
+  element.text(text);
+};
+
+function renderNextButtonText(state, element) {
+    var text = state.currentQuestionIndex < state.questions.length - 1 ?
+      "Next" : "How did I do?";
+  element.text(text);
+};
+
+function renderFinalFeedbackText(state, element) {
+  var text = "You got " + state.score + " out of " +
+    state.questions.length + " questions right.";
+  element.text(text);
+};
+
+// Event handlers
+var PAGE_ELEMENTS = {
+  'start': $('.start-page'),
+  'question': $('.question-page'),
+  'answer-feedback': $('.answer-feedback-page'),
+  'final-feedback': $('.final-feedback-page')
+};
+
+$("form[name='game-start']").submit(function(event) {
+  event.preventDefault();
+  setRoute(state, 'question');
+  renderApp(state, PAGE_ELEMENTS);
+});
+
+$(".restart-game").click(function(event){
+  event.preventDefault();
+  resetGame(state);
+  renderApp(state, PAGE_ELEMENTS);
+});
+
+$("form[name='current-question']").submit(function(event) {
+  event.preventDefault();
+  var answer = $("input[name='user-answer']:checked").val();
+  answer = parseInt(answer, 10);
+  answerQuestion(state, answer);
+  renderApp(state, PAGE_ELEMENTS);
+});
+
+$(".see-next").click(function(event) {
+  advance(state);
+  renderApp(state, PAGE_ELEMENTS);
+});
+
+$(function() { renderApp(state, PAGE_ELEMENTS); });
